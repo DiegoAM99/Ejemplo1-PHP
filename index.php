8617abf914e9d2fa555a20a794be62b1029caaec
Administrator <Administrator@pc.localdomain>
2018-09-20 17:15:42 +0200

    Comenzamos con un ejemplo

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PRUEBA DE PHP CON BOOTSTRAP</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <input name="usuario_nombre" type="text" placeholder="Usuario">
+                </div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+</html>
