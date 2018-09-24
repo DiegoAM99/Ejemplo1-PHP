d9a4d38778b260106fc91a1deef81d9d24eec602
Administrator <Administrator@pc.localdomain>
2018-09-24 19:35:14 +0200

    Esto va estando mejor

--- index.php
+++ index.php
@@ -17,14 +17,16 @@
                 <div class="col-4">
                    
                 </div>
-                <br><br/>
                 <div class="col-4">
-                    <input id="cajaNombre" class="form-control"  type="text" placeholder="Usuario">
-                    <br/>
-                    <input id="cajaPassword" class="form-control" type="text" placeholder="Contraseña">
-                    <br/>
-                    <button id= "boton1" class="btn btn-primary btn-block">Primary</button>
                     <br/><br/>
+                    <form action="index2.php">
+                        <input id="cajaNombre" class="form-control"  type="text" placeholder="Usuario" required="required">
+                    <br/>
+                    <input id="cajaPassword" class="form-control" type="password" placeholder="Contraseña" required="required">
+                    <br/>
+                    <button id= "boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
+                    <br/><br/>
+                    </form>
                 </div>
                 <div class="col-4"></div>
             </div>
@@ -36,14 +38,14 @@
     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
     
     <script>
-        // document ready se ejecuta cuando toda la página se ha cargado correctamente
+         document ready se ejecuta cuando toda la página se ha cargado correctamente
         $(document).ready(function (){
             $('#cajaNombre').hide();
         });
-        
-        $('#boton1').click(function(){
-            console.log("aqui si que llega");
-            $('#cajaNombre').show(200);
-        });
+//        
+//        $('#boton1').click(function(){
+//            console.log("aqui si que llega");
+//            $('#cajaNombre').show(200);
+//        });
     </script>
 </html>
